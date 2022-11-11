6c574df59ec8577026ed17ee1ff157551bde680f
Andressa Da Costa <andressadacostan@gmail.com>
2022-11-11 10:04:29 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Cabecalho from "./Components/Cabecalho";
+import Container from "./Components/Container";
+
+export function App() {
+  return (
+    <>
+      <Cabecalho />
+      <Container />
+    </>
+  );
+}
