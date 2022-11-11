24d9d22b682ee84c3fa75f900dafed40e588f3f0
Andressa Da Costa <andressadacostan@gmail.com>
2022-11-11 10:34:36 -0300

    refatorando

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './assets/estilos.css';
+// import './assets/estilos.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
